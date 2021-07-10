4755e255d35c13584f5a567eb347c0941631bc46
AhmedELKhateebSafe <ahmed.elkhateeb@safe-sas.com>
2021-07-10 18:50:49 +0200

    adding corsConfigurer

--- src/main/java/com/demo/customers/CustomersApplication.java
+++ src/main/java/com/demo/customers/CustomersApplication.java
@@ -2,12 +2,29 @@ package com.demo.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CustomersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("PUT", "DELETE", "GET", "POST")//or allow all as you like
+                        .allowCredentials(true)
+                        .allowedHeaders("Content-Type", "Accept", "Authorization")
+                        .exposedHeaders("header1", "header2");
+            }
+        };
+    }
 }
